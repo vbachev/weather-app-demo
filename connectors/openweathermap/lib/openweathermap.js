489269c35b652c359ecb1f671d05cf34443aeaea
Vasil Bachev <vasil.bachev@ecommera.com>
2016-10-13 12:09:03 +0300

    fixing the url generation for the request to the OWM API

--- connectors/openweathermap/lib/openweathermap.js
+++ connectors/openweathermap/lib/openweathermap.js
@@ -7,21 +7,22 @@ var OWMService = function (config) {
         '&appid=' + config.api_key;
 
     function getForecast (params, callback) {
+        var requestUrl = service_url;
         
         if(params && params.lat){
             // search by geolocation
-            service_url += '&lat=' + params.lat + '&lon=' + params.lon;
+            requestUrl += '&lat=' + params.lat + '&lon=' + params.lon;
 
         } else if(params && params.city){
             // search by city name
-            service_url += '&q=' + params.city;
+            requestUrl += '&q=' + params.city;
 
         } else {
             // search for the default city
-            service_url += '&q=' + config.default_city;
+            requestUrl += '&q=' + config.default_city;
         }
 
-        request(service_url, function (error, response, body) {
+        request(requestUrl, function (error, response, body) {
             if(error || response.statusCode != 200){
                 return callback(error || 'Response code ' + response.statusCode);
             }
